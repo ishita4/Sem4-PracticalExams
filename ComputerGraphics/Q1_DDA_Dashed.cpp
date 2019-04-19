29d79240e9c003a5bf38e8b4e5c3723bba06d03b
ishita4 <43470474+ishita4@users.noreply.github.com>
2019-04-19 20:51:26 +0530

    Create Q1_DDA_Dashed.cpp

---
+++ ComputerGraphics/Q1_DDA_Dashed.cpp
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <dos.h>
+#include <graphics.h>
+#include <math.h>
+#include <stdlib.h>
+
+void drawLine(int x1, int y1, int x2, int y2)
+{
+    int dx,dy,steps,i;
+    float x,y,xInr,yInr;
+    dx=(x2-x1);
+    dy=(y2-y1);
+
+    steps=abs(dx)>abs(dy)?abs(dx):abs(dy);
+
+    xInr= ((float)dx/steps);
+    yInr= ((float)dy/steps);
+
+    x=x1;
+    y=y1;
+
+    for(i=0;i<=steps;i++)
+    {
+        if(i%5<3) //Prints 3 consecutive points, leaves out the next 2 points and repeats
+        {
+            putpixel(round(x),round(y),WHITE);
+        }
+        x += xInr;
+        y += yInr;
+        delay(50);
+
+    }
+}
+
+int main()
+{
+    int gd=DETECT, gm;
+    initgraph(&gd,&gm,"C:\\TurboC3\\BGI");
+    int x1,y1,x2,y2;
+
+    x1=200;
+    y1=200;
+    x2=400;
+    y2=400;
+
+    drawLine(x1,y1,x2,y2);
+
+    getch();
+    closegraph();
+
+    return 0;
+}
